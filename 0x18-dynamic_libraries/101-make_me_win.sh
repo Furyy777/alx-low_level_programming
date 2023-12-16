09088e97560a08cf968f99130cc0593fefeaf83a
Anassselasri <anaselasri37@gmail.com>
2023-12-16 03:40:34 +0100

    dynamic     lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o libgiga.so -fPIC test.c
-export LD_PRELOAD=/tmp/libgiga.so:$LD_LIBRARY-PATH
+wget -P https://github.com/Furyy777/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
